bca4817cb017204ea4a2ee154d6bbaa074bcbece
teklaturmanidze92-prog <teklaturmanidze92@gmail.com>
2025-11-06 17:37:14 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+import json, os
+
+app = Flask(__name__)
+POSTS_FILE = 'posts.json'
+
+# Load posts from file
+def load_posts():
+    if not os.path.exists(POSTS_FILE):
+        return []
+    with open(POSTS_FILE, 'r') as f:
+        return json.load(f)
+
+# Save posts to file
+def save_posts(posts):
+    with open(POSTS_FILE, 'w') as f:
+        json.dump(posts, f, indent=4)
+
+# Home page: show all posts
+@app.route('/')
+def index():
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
+
+# New post page: form to create post
+@app.route('/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        posts = load_posts()
+        posts.append({'title': title, 'content': content})
+        save_posts(posts)
+        return redirect('/')
+    return render_template('new.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
